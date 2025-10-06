0412ea8e0f919511c319697a1407a87c60a9ba96
idil-abdi <edilabdiaziz@gmail.com>
2025-10-06 18:13:02 +0100

    first commit

--- app.js
+++ app.js
@@ -95,20 +95,20 @@
 // })   
 
 // ? Http modules
+// create my own http on browser
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/') {
+//     res.end('Welcome to our home page')
+//     } else if (req.url === '/about') {
+//         res.end('Here is our short history')
+//     } else {
+//         res.end(`
+//             <h1>Oops!</h1>
+//             <p>We can't seem to find the page you are looking for</p>
+//             <a href="/">back home</a>
+//         `)
+//     }
+// })
 
-const http = require('http')
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-    res.end('Welcome to our home page')
-    } else if (req.url === '/about') {
-        res.end('Here is our short history')
-    } else {
-        res.end(`
-            <h1>Oops!</h1>
-            <p>We can't seem to find the page you are looking for</p>
-            <a href="/">back home</a>
-        `)
-    }
-})
-
-server.listen(5000)
+// server.listen(5000)
